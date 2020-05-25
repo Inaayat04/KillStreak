9e326c7cad33268d5a8b8a65d6706351d67e9bad
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-25 11:52:16 +0400

    Create SQLiteProvider.php

---
+++ src/Inaayat/provider/SQLiteProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Inaayat\provider;
+
+use Inaayat\KillStreak;
+use pocketmine\Player;
+
+class SQLiteProvider implements ProviderInterface {
+
+    /** @var \SQLite3 */
+    public $killstreakdb;
+
+    public function prepare(): void{
+        $this->killstreakdb = new \SQLite3(KillStreak::getInstance()->getDataFolder() . "killstreak.db");
+        $this->killstreakdb->exec("CREATE TABLE IF NOT EXISTS master (player TEXT PRIMARY KEY COLLATE NOCASE, ks INT)");
+    }
+
+    public function registerPlayer(Player $player): void{
+        $stmt = $this->killstreakdb->prepare("INSERT OR REPLACE INTO master (player, ks) VALUES (:player, :ks)");
+        $stmt->bindValue(":player", $player->getLowerCaseName());
+        $stmt->bindValue(":ks", "0");
+        $stmt->execute();
+    }
+
+    public function addKSPoints(Player $player, int $points = 1): void{
+        $stmt = $this->killstreakdb->prepare("INSERT OR REPLACE INTO master (player, kills, deaths) VALUES (:player, :kills, :deaths)");
+        $stmt->bindValue(":player", $player->getLowerCaseName());
+        $stmt->bindValue(":ks", $this->getPlayerKSPoints($player) + $points);
+        $stmt->execute();
+    }
+
+    public function resetKSPoints(Player $player): int{
+        $stmt = $this->killstreakdb->prepare("INSERT OR REPLACE INTO master (player, kills, deaths) VALUES (:player, :kills, :deaths)");
+        $stmt->bindValue(":player", $player->getLowerCaseName());
+        $stmt->bindValue(":ks", "0");
+        $stmt->execute();
+    }
+
+    public function playerExists(Player $player): bool{
+        $playerName = $player->getLowerCaseName();
+        $result = $this->killstreakdb->query("SELECT player FROM master WHERE player='$playerName';");
+        $array = $result->fetchArray(SQLITE3_ASSOC);
+        return empty($array) == false;
+    }
+
+    public function getPlayerKSPoints(Player $player): int{
+        $playerName = $player->getLowerCaseName();
+        $result = $this->killstreakdb->query("SELECT kills FROM master WHERE player = '$playerName'");
+        $resultArray = $result->fetchArray(SQLITE3_ASSOC);
+        return (int) $resultArray["ks"];
+    }
+
+    public function close(): void{
+        $this->killstreakdb->close();
+    }
+}
