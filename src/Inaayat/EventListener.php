d0cbf952fe0723c01920ca38aa65f968f206e563
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-25 11:52:49 +0400

    Create EventListener.php

---
+++ src/Inaayat/EventListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Inaayat;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+
+class EventListener implements Listener{
+
+    public $plugin;
+
+    public function __construct(KillStreak $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        if(!$this->plugin->getProvider()->playerExists($player)){
+            $this->plugin->getProvider()->registerPlayer($player);
+        }
+    }
+
+    public function onPlayerKill(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        if($player instanceof Player){
+            $this->plugin->getProvider()->resetKSPoints($player);
+        }
+        $cause = $player->getLastDamageCause();
+        if($cause instanceof EntityDamageByEntityEvent){
+            $d = $cause->getDamager();
+            if($d instanceof Player){
+                $this->plugin->getProvider()->addKSPoints($d, (int)"1");
+            }
+        }
+    }
+}
