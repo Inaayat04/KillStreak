2dbfa7d7d68fa3714d6762b66621a3ad51c1edf0
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-25 17:22:04 +0400

    Create KillStreakScoreHudAddon.php

---
+++ KillStreakScoreHudAddon.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @name KillStreakAddon
+ * @version 1.0.0
+ * @main JackMD\ScoreHud\Addons\KillStreakAddon
+ * @depend KillStreak
+ */
+namespace JackMD\ScoreHud\Addons
+{
+
+	use JackMD\ScoreHud\addon\AddonBase;
+	use Inaayatt\KillStreak;
+	use pocketmine\Player;
+
+	class KillStreakAddon extends AddonBase{
+
+		/** @var KillStreak */
+		private $KillStreak;
+
+		public function onEnable(): void{
+			$this->KillStreak = $this->getServer()->getPluginManager()->getPlugin("KillStreak");
+		}
+
+		/**
+		 * @param Player $player
+		 * @return array
+		 */
+		public function getProcessedTags(Player $player): array{
+			return [
+				"{killstreak}"       => $this->getPlayerKS($player)
+			];
+		}
+
+		/**
+		 * @param Player $player
+		 * @return string
+		 */
+		public function getPlayerKS(Player $player): string{
+			$ks = $this->KillStreak;
+			return $ks->getProvider()->getPlayerKSPoints($player);
+		}
+}
