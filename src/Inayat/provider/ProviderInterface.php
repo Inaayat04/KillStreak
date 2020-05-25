2c26213be2562199740e7d5b217938035664f0aa
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-25 11:49:16 +0400

    Create ProviderInterface.php

---
+++ src/Inayat/provider/ProviderInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Inaayat\provider;
+
+use pocketmine\Player;
+
+interface ProviderInterface {
+
+    public function prepare(): void;
+
+    public function registerPlayer(Player $player): void;
+
+    public function addKSPoints(Player $player, int $points = 1): void;
+
+    public function playerExists(Player $player): bool;
+
+    public function getPlayerKSPoints(Player $player): int;
+
+    public function resetKSPoints(Player $player): int;
+
+    public function close(): void;
+}
